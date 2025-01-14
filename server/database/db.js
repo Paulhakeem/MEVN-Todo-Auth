baf2849cff01e9b836e79e1b730d8161f560e36f
Paul <paulnyamawi18@gmail.com>
2025-01-14 21:07:36 +0300

    bulding backend

--- server/database/db.js
+++ server/database/db.js
@@ -2,3 +2,25 @@ const express = require('express')
 const env = require('dotenv')
 const bodyparser = require('body-parser')
 const mongoose = require('mongoose')
+
+
+const app = express()
+
+env.config('./env')
+
+app.use(bodyparser.json())
+
+// login route
+app.get('/login', (req, res)=>{
+    res.status(200).json({
+        status: 200,
+        text: "hello "
+    })
+})
+
+
+
+// server
+app.listen(5000, ()=> {
+    console.log('server is running....');
+})
