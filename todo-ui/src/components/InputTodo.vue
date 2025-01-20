72f543caa31afc9f357c3929b697480618e8328d
Paul <paulnyamawi18@gmail.com>
2025-01-20 21:35:32 +0300

    done

--- todo-ui/src/components/InputTodo.vue
+++ todo-ui/src/components/InputTodo.vue
@@ -5,11 +5,11 @@
       <input
         type="text"
         placeholder="Enter your todo..."
-        class="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm pl-2"
+        class="w-full rounded-md border-gray-200 outline-none py-2.5 pe-10 shadow-sm sm:text-sm pl-2 hover:outline-gray-400"
       />
     
       <span class="absolute inset-y-0 end-0 grid w-10 place-content-center">
-        <button type="button" class="text-white bg-blue-400 p-1 rounded-md">
+        <button type="button" class="text-white bg-purple-300 p-1 rounded-md">
          Add
         </button>
       </span>
